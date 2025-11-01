2e21f605ffb2fbfb7362d6404acaee62a2404635
ndntu9 <ndntu.629@gmail.com>
2025-11-02 00:47:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useRef, useState } from "react";
+import OpenAI from "openai";
+import "./App.css";
+
+const client = new OpenAI({
+    baseURL: "https://token.ai.vn/v1",
+    apiKey: "sk-a9EBG4GdNLmhBk2_fkpxsCiWutoR1ZfCeNn2NKMTZ1DD8BostLPILlQyYrg",
+    dangerouslyAllowBrowser: true
+});
+
+function App() {
+    const inputRef = useRef();
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const messageWithUserMessage = [
+            ...messages,
+            {
+                role: "user",
+                content: message
+            }
+        ];
+        setMessages(messageWithUserMessage);
+        setMessage("");
+        inputRef.current.focus();
+
+        const chatCompletion = await client.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: messageWithUserMessage
+        });
+        const botMessage = chatCompletion.choices[0].message.content;
+
+        setMessages([
+            ...messageWithUserMessage,
+            {
+                role: "assistant",
+                content: botMessage
+            }
+        ]);
+    }
+
+    return (
+        <main className="main">
+            <h1 className="heading">Chat UI với React + OpenAI</h1>
+            <form
+                className="form"
+                onSubmit={(e) => {
+                    handleSubmit(e);
+                }}
+            >
+                <input
+                    type="text"
+                    id="msg"
+                    className="input"
+                    value={message}
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }}
+                    placeholder="Tin nhắn của bạn..."
+                    ref={inputRef}
+                />
+                <button type="submit" className="submit">
+                    Gửi tin nhắn
+                </button>
+            </form>
+            <div className="content">
+                {messages.length > 0 &&
+                    messages.map((message, index) => (
+                        <p
+                            className={`message ${
+                                message.role === "user" ? "user" : "assistant"
+                            }`}
+                            key={index}
+                        >
+                            {message.content}
+                        </p>
+                    ))}
+            </div>
+        </main>
+    );
+}
+
+export default App;
